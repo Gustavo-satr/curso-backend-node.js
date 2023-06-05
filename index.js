842f6e490d8c0b8363c165b146908af5e520e1db
Gustavo Barrera <gxsbarrera@gmail.com>
2023-06-04 21:23:37 -0600

    Routing con Express

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
-})
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name : "Producto",
+    price : 2000
+  });
+});
 
 app.listen(port, () => {
   console.log('Mi port ' + port);
