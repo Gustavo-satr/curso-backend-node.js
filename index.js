362f37587f849ae5e4694f652a03c1a2d1cef20f
Gustavo Barrera <gxsbarrera@gmail.com>
2023-06-04 23:36:43 -0600

    POST: método para crear

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const routerApi = require('./routes');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
@@ -36,6 +38,7 @@ routerApi(app);
 // })
 
 app.listen(port, () => {
+
   console.log('Mi port' +  port);
 
 });
