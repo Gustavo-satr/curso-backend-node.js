7e952ea9053d1a6274bd4d6b008ef1dbc7d184c0
Gustavo Barrera <gxsbarrera@gmail.com>
2023-06-06 22:14:12 -0600

    Middleware para HttpErrors

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ProductsService = require('./../services/product.service');
 
+
 const router = express.Router();
 const service = new ProductsService();
 
@@ -13,12 +14,14 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy un filter');
 });
 
-router.get('/:id', async (req, res) => {
-
-  const { id } = req.params;
-  const product = await service.findOne(id);
-  res.json(product);
-
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await service.findOne(id);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res) => {
