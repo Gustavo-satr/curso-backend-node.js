a7831f1be2639da47eada9a257aee62219a19617
Gustavo Barrera <gxsbarrera@gmail.com>
2023-06-06 22:00:43 -0600

    Async await y captura de errores

--- service/product.service.js
+++ service/product.service.js
@@ -2,60 +2,64 @@ const { faker } = require('@faker-js/faker');
 
 class ProductsService{
   constructor(){
-    this.products=[];
+    this.products = [];
     this.generate();
-}
-generate(){
-  const limit=100;
-  for(let index=0;index<limit;index++){
-    this.products.push({
-      id:faker.string.uuid(),
-      name:faker.commerce.productName(),
-      price:parseInt(
-        faker.commerce.price(),
-        10),
-        image:faker.image.imageUrl(),
+  }
+
+  generate() {
+    const limit = 100;
+    for (let index = 0; index < limit; index++) {
+      this.products.push({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(), 10),
+        image: faker.image.imageUrl(),
       });
     }
   }
-  create(data){
-    const newProduct = {
-      id: faker.string.uuid(),
+      async create(data) {
+      const newProduct = {
+      id: faker.datatype.uuid(),
       ...data
+        }
+      this.products.push(newProduct);
+      return newProduct;
     }
-    this.products.push(newProduct);
-    return newProduct;
-  }
+      find() {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(this.products);
+          }, 5000);
+        })
+      }
 
-find(){
-  return this.products;
-}
+      async findOne(id) {
+        return this.products.find(item => item.id === id);
+      }
 
-findOne(id){
-  return this.products.find(item => item.id === id);
-}
+      async update(id, changes) {
+        const index = this.products.findIndex(item => item.id === id);
+        if (index === -1) {
+          throw new Error('product not found');
+        }
 
-update(id){
-  const index = this.products.findIndex(item => item.id === id);
-  if (index === -1) {
-    throw new Error('product not found');
-  }
-  const product = this.products[index];
-  this.products[index] = {
-    ...product,
-    ...changes
-  };
-  return this.products[index];
+        const product = this.products[index];
+        this.products[index] = {
+          ...product,
+          ...changes
+        };
+        return this.products[index];
+      }
 
-}
+      async delete(id) {
+        const index = this.products.findIndex(item => item.id === id);
+        if (index === -1) {
+          throw new Error('product not found');
+        }
+        this.products.splice(index, 1);
+        return { id };
+      }
 
-delete(id){
-  const index = this.products.findIndex(item => item.id === id);
-  if (index === -1) {
-    throw new Error('product not found');
-  }
-  this.products.splice(index, 1);
-  return { id };
-}
-}
-module.exports=ProductsService;
+    }
+  module.exports = ProductsService;
+
